b9b4cb6e51e3e1bd436e6c689e1a68b9239f99f5
zingten11 <admin@zingalicio.us>
2014-07-23 23:55:35 -0700

    Actually created EvansUtil

---
+++ src/main/java/us/zingalicio/handlefish/util/EvansUtil.java
@@ -0,0 +1,12 @@
+package us.zingalicio.handlefish.util;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class EvansUtil 
+{
+	public static Player getEvans()
+	{
+		return Bukkit.getServer().getPlayer("masterlink0");
+	}
+}
